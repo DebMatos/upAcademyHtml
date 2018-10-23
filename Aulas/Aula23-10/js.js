46a0bc8ca88faefedc0b919af9829e71473e79e3
DebMatos <matos.debora88@gmail.com>
2018-10-23 20:13:28 +0100

    funcoes

---
+++ Aulas/Aula23-10/js.js
@@ -0,0 +1,68 @@
+
+
+
+// funcoes para o index.html
+// conta os espacoes e mostra 
+function mostrar() {
+
+
+    var input1 = document.getElementById("text").value;
+    var input2 = document.getElementById("text2").value;
+
+    document.getElementById("hConta1").innerHTML = contar(input1);
+    document.getElementById("hConta2").innerHTML = contar(input2)
+}
+
+function contar(x) {
+
+
+    var count = 0;
+    for (var i = 0; i < x.length; i++) {
+        if (x[i] == " ") {
+            count += 1;
+        }
+
+    }
+    return count;
+
+
+}
+
+//ordena
+// var teste = 0;
+function ordena(num) {
+    // teste++;
+    // if (teste > 5) {
+    //     return console.log("a");
+    // }
+
+    var verif = false;
+    for (var i = 0; i < num.length - 1; i++) {
+
+        if (parseInt(num[i]) < parseInt(num[i + 1])) {
+            verif = true;
+            var extra = num[i];
+            num[i] = num[i + 1];
+            num[i + 1] = extra;
+
+
+        }
+
+    }
+    if (verif == true) {
+        return ordena(num)
+    } else {
+        return num;
+    }
+
+}
+
+function mostraResultado() {
+
+
+    var ordenar = document.getElementById("ordenar").value;
+
+
+    document.getElementById("ordenados").innerHTML = ordena(ordenar.split(""));
+
+}
