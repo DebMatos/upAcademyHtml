f5c1f657b7db9e9e871a7367103e2f779dcdbc37
DebMatos <matos.debora88@gmail.com>
2018-10-25 18:56:52 +0100

    UpdateAjudanteProf

--- Aulas/Aula24-10/Js/prof.js
+++ Aulas/Aula24-10/Js/prof.js
@@ -1,30 +1,31 @@
 
-
-$(document).ready(function () {
-    console.log("ready!");
-
-});
-
 var numAlunos;
 var contaClique = 0;
-var ListaAlunos = [];
-var numero=0;
+var listaAlunos = [];
+var numero = 0;
+var notaRef=0;
+var media = 0;
 
-function Aluno(numero, nota) {
-    this.numero = numero;
-    this.nota = nota;
+class Aluno {
+    constructor(numero, nota) {
+        this.numero = numero;
+        this.nota = nota;
+    }
 }
+
+
 
 
 $("button").click(function () {
     contaClique++;
     Nalunos();
     CriaAlunos();
+
     console.log(numAlunos);
     console.log(contaClique);
-    console.log(ListaAlunos);
+    console.log(listaAlunos);
     $("input").val("");
-  
+$("input").focus();
 });
 
 function Nalunos() {
@@ -52,24 +53,30 @@ function CriaAlunos() {
         AlunoNovo.numero = contaClique - 1;
         AlunoNovo.nota = parseInt($("input").val());
 
-        ListaAlunos.push(AlunoNovo);
+        listaAlunos.push(AlunoNovo);
 
         $("#divTab").html(getTable());
         $("#divTab").html(novaLinha());
 
 
     }
-
-    if (contaClique > numAlunos) {
-        $("#part1").hide();
+    if (contaClique==numAlunos+1) {
+        $("#lblDinamica").text("Insira a nota do numero de referencia:");
+        notaRef=parseInt($("input").val()); 
+    }
+    if (contaClique>numAlunos+1) {
+        
+       
         // $("#lblDinamica").hide();
-        // $("#ipt").hide();
+        $("#mostraMedia").html("A media é "+ Media()+ " sendo a nota de referencia "+ notaRef+" entao existe uma variacao de " +Desvio()+"%");
+        $("#part1").hide();
+        $("#mostraMedia").append( '<p class="text-center" ><button id="btnSub" class="btn btn-success">Refresh</button></p>');
     }
 
 }
 
-function getTable() {
-
+function getTable(num) {
+    var str;
     return `
     <table class="table table-hover text-center">
     <thead class="thead-dark ">
@@ -80,14 +87,40 @@ function getTable() {
     </tr>
    </thead>
   </table>`
-  ;
+        ;
 }
 
 function novaLinha() {
-for (let index = 0; index < ListaAlunos.length; index++) {
-     $("table").append("<tr class='table-hover text-center'><td>"+ListaAlunos[index].numero+"</td><td>"+ListaAlunos[index].nota+"</td></tr>");
-    
-}
-   
+    for (let index = 0; index < listaAlunos.length; index++) {
+        $("table").append("<tr class='table-hover text-center'><td>" + listaAlunos[index].numero + "</td><td>" + listaAlunos[index].nota + "</td></tr>");
+
+    }
+
+
+
 }
 
+function Media() {
+ 
+
+    for (var i=0; i < listaAlunos.length; i++) {
+
+       media += parseInt(listaAlunos[i].nota);
+    }
+    console.log(media,listaAlunos.l);
+     return (media / listaAlunos.length);
+    
+}
+
+
+function Desvio() {
+   
+var desvioRef=(media-notaRef)/media*100;
+    
+     return desvioRef;
+    
+}
+
+function RefreshPag() {
+    location.reload();
+}
